edf52be11f0cf670a169865f31c9bf82ff5169a4
Mohammed Mulazada <thisismoniac@gmail.com>
2018-04-12 16:20:56 +0200

    loads tweets and analyzes

--- public/socket.js
+++ public/socket.js
@@ -4,6 +4,10 @@ socket.on('connect', () => {
 	socket.emit('join')
 })
 
+socket.on('result', (result) => {
+	console.log('yay', result)
+})
+
 const input = document.querySelector('input')
 
 input.addEventListener('mouseleave', function(e) {
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const dotenv = require('dotenv').config()
 const brain = require('brain.js')
 const Twitter = require('twitter')
-let net = new brain.NeuralNetwork();
-let trainedNet;
-let longest;
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -12,13 +9,15 @@ const server = http.createServer(app)
 const io = socketIO(server);
 const path = require('path')
 let dataTweet = []
+let result
+let ok = 'hoi'
 app.set('view engine', 'ejs')
 const publicPath = path.join(__dirname, './public')
 app.use(express.static(publicPath))
 
-
-const params = { screen_name: 'realdonaldtrump', count: 10, result_type: 'recent', tweet_mode: 'extended' };
-const paramsObama = { screen_name: 'barackobama', count: 10, result_type: 'recent', tweet_mode: 'extended' };
+let net = new brain.NeuralNetwork();
+let trainedNet;
+let longest;
 
 const tokens = {
 	consumer_key: process.env.CONSUMERKEY,
@@ -26,80 +25,93 @@ const tokens = {
 	access_token: process.env.access_token,
 	access_token_key: process.env.access_token_key,
 	access_token_secret: process.env.access_token_secret
-  }
-
-app.get('/', (req, res) => {
-	console.log(trainedNet(encode(adjustSize('Last night, it was my great honor to host America’s senior defense and military leaders for dinner at the White House. America’s military is the GREATEST fighting force in the history of the world. They all have my pledge of unwavering commitment to our men and women in uniform! '))));
-	res.render('index')
-})
-
-io.on('connection', (socket) => {
-    socket.on('join', (message) => {
-    })
-})
-
-io.on('connection', (socket) => {
-    socket.on('new message', (message) => {
-		console.log(message)
-    })
-})
-
-server.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
-})
+}
 
 const client = new Twitter(tokens)
 
-getTweets( () => {
-
-	train(getTrainingData())
-
-	console.log(trainedNet(encode(adjustSize('Incredible to have a Chicago team in the Final Four. I’ll take that over an intact bracket any day! Congratulations to everybody @loyolachicago - let’s keep it going!'))));
-	console.log(trainedNet(encode(adjustSize('Last night, it was my great honor to host America’s senior defense and military leaders for dinner at the White House. America’s military is the GREATEST fighting force in the history of the world. They all have my pledge of unwavering commitment to our men and women in uniform! '))));
-
-} )
-
-async function getTweets ( cb ) {
-
-    const promise1 = await client.get('statuses/user_timeline', paramsObama)
-    const promise2 = await client.get('statuses/user_timeline', params)
-    
-    Promise.all([promise1, promise2]).then((data) => {
-    const obama = data[0];
-	const trump = data[1];
-
-        obama.forEach(tweet => {
-            dataTweet.push({
-                input: tweet.full_text.split('https:')[0],
-                output: {[tweet.user.name.split(' ')[0]]: 1}
-            })
-        });
-        trump.forEach(tweet => {
-            dataTweet.push({
-                input: tweet.full_text.split('https:')[0],
-                output: {[tweet.user.name.split(' ')[0]]: 1}
-            })
-        });
-    }).then(() => {
-		train(dataTweet, cb)
-    }).catch((e) => {
-		console.log(e)
+function getTweets(user) {
+	const promise = new Promise((resolve, reject) => {
+		const params = { screen_name: `${user}`, count: 50, result_type: 'recent', tweet_mode: 'extended' };
+		console.log(params)
+		client.get('statuses/user_timeline', params)
+		.then((data) => {
+			if (data) {
+				resolve(data)
+			}
+		})
 	})
+	
+	return promise
 }
 
-function train(data, cb) {
+getTweets('barackobama')
+	.then((data) => {
+		data.forEach((tweet) => {
+			dataTweet.push({
+				input: tweet.full_text.split('https:')[0],
+				output: {[tweet.user.name.split(' ')[0]]: 1}
+			})
+		})
+	}).then(() => {
+		// train(getTrainingData(dataTweet))
+		console.log(dataTweet)
+	}).then(() => {
+		
+		getTweets('realdonaldtrump').then((data) => {
+			data.forEach((tweet) => {
+				dataTweet.push({
+					input: tweet.full_text.split('https:')[0],
+					output: {[tweet.user.name.split(' ')[0]]: 1}
+				})
+			})
+		}).then(() => {
+			train(getTrainingData(dataTweet))
+			console.log(trainedNet(encode(adjustSize('Last night, it was my great honor to host America’s senior defense and military leaders for dinner at the White House. America’s military is the GREATEST fighting force in the history of the world. They all have my pledge of unwavering commitment to our men and women in uniform! '))));
+
+			console.log(trainedNet(encode(adjustSize('Incredible to have a Chicago team in the Final Four. I’ll take that over an intact bracket any day! Congratulations to everybody @loyolachicago - let’s keep it going!'))));
+		})
+	}).catch((e) => {
+		console.log(e)
+	})
+
+	app.get('/', (req, res) => {
+		console.log(dataTweet)
+		res.render('index', {
+			result: result,
+			ok: ok
+		})
+	})
+	
+	io.on('connection', (socket) => {
+		socket.on('join', (message) => {
+		})
+	})
+	
+	io.on('connection', (socket) => {
+		socket.on('new message', (message) => {
+			result = trainedNet(encode(adjustSize(`${message}`)))
+			console.log(result, 2)
+			console.log(trainedNet(encode(adjustSize(`${message}`))));
+			socket.emit('result', result)
+		})
+	})
+	
+	server.listen(3000, () => {
+	  console.log('Example app listening on port 3000!')
+	})
+
+// console.log(trainedNet(encode(adjustSize('Last night, it was my great honor to host America’s senior defense and military leaders for dinner at the White House. America’s military is the GREATEST fighting force in the history of the world. They all have my pledge of unwavering commitment to our men and women in uniform! '))));
+
+// console.log(trainedNet(encode(adjustSize('Incredible to have a Chicago team in the Final Four. I’ll take that over an intact bracket any day! Congratulations to everybody @loyolachicago - let’s keep it going!'))));
+
+function train(data) {
     net.train(processTrainingData(data), {
-        iterations: 20,
+        iterations: 2000,
         log:true,
-		learningRate: 0.1,
-		logPeriod:10,
+        learningRate: 0.1,
         timeout: 1000
     });
-	trainedNet = net.toFunction();
-	
-	if ( cb ) {
-		cb()
-	}
+    trainedNet = net.toFunction();
 }
 
 function encode(arg) {
@@ -113,54 +125,18 @@ function processTrainingData(data) {
             output: d.output
         }
     });
+    // console.log(processedValues);
     return processedValues;
 }
 
 function getTrainingData(data) {
-  console.log(dataTweet,'now')
-	const trainingData = [{
-    input: 'RT @ObamaFoundation: This week—50 years since Dr. Martin Luther King, Jr. was killed—@BarackObama and @repjohnlewis sat down with a group o…',
-    output: {
-        Barack: 1
-    }
-  },
-    {
-      input: 'Incredible to have a Chicago team in the Final Four. I’ll take that over an intact bracket any day! Congratulations to everybody @loyolachicago - let’s keep it going!',
-      output: {
-        Barack: 1
-      }
-    },
-    {
-      input: 'In Singapore with young people who are advocating for education, empowering young women, and getting involved all over Southeast Asia with a profoundly optimistic commitment to building the world they want to see. ',
-      output: {
-        Barack: 1
-      }
-    },
-    {
-      input: 'Very thankful for President Xi of China’s kind words on tarrifs and automobile barriers...also, his enlightenment on intellectual property and technology transfers. We will make great progress together!',
-      output: {
-        Donald: 1
-      }
-    },
-    {
-      input: 'Last night, it was my great honor to host America’s senior defense and military leaders for dinner at the White House. America’s military is the GREATEST fighting force in the history of the world. They all have my pledge of unwavering commitment to our men and women in uniform! ',
-      output: {
-        Donald: 1
-      }
-    },
-    {
-      input: 'A TOTAL WITCH HUNT!!!',
-      output: {
-        Donald: 1
-      }
-    }
-  ];
-  longest = dataTweet.reduce((a, b) =>
+	const trainingData = data
+  longest = trainingData.reduce((a, b) =>
     a.input.length > b.input.length ? a : b).input.length;
-  for (let i = 0; i < dataTweet.length; i++) {
-    dataTweet[i].input = adjustSize(dataTweet[i].input);
+  for (let i = 0; i < trainingData.length; i++) {
+    trainingData[i].input = adjustSize(trainingData[i].input);
   }
-  return dataTweet;
+  return trainingData;
 }
 
 function adjustSize(string) {
